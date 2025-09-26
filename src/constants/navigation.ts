a5a4a43a3746e27e6d8128ed7ac0fd23b71314a7
syahfrinorezky <syahfrinorezky@gmail.com>
2025-09-26 20:43:02 +0800

    chore: add navigations constant

---
+++ src/constants/navigation.ts
@@ -0,0 +1,123 @@
+import { IconType } from "react-icons";
+import {
+  IoHome,
+  IoHomeOutline,
+  IoCar,
+  IoCarOutline,
+  IoStar,
+  IoStarOutline,
+  IoPersonSharp,
+  IoPersonOutline,
+  IoTicket,
+  IoTicketOutline,
+} from "react-icons/io5";
+
+export type NavigationItem = {
+  name: string;
+  href: string;
+  Icon: IconType;
+  ActiveIcon: IconType;
+};
+
+const publicNavigation: NavigationItem[] = [
+  {
+    name: "Home",
+    href: "/",
+    Icon: IoHomeOutline,
+    ActiveIcon: IoHome,
+  },
+  {
+    name: "Vehicles",
+    href: "/vehicles",
+    Icon: IoCarOutline,
+    ActiveIcon: IoCar,
+  },
+  {
+    name: "Reviews",
+    href: "/reviews",
+    Icon: IoStarOutline,
+    ActiveIcon: IoStar,
+  },
+];
+
+const userNavigation: NavigationItem[] = [
+  {
+    name: "Profile",
+    href: "/profile",
+    Icon: IoPersonOutline,
+    ActiveIcon: IoPersonSharp,
+  },
+  {
+    name: "Dashboard",
+    href: "/user-dashboard",
+    Icon: IoHomeOutline,
+    ActiveIcon: IoHome,
+  },
+  {
+    name: "Bookings",
+    href: "/bookings",
+    Icon: IoTicketOutline,
+    ActiveIcon: IoTicket,
+  },
+];
+
+const adminNavigation: NavigationItem[] = [
+  {
+    name: "Dashboard",
+    href: "/admin-dashboard",
+    Icon: IoHomeOutline,
+    ActiveIcon: IoHome,
+  },
+  {
+    name: "Manage Vehicles",
+    href: "/manage-vehicles",
+    Icon: IoCarOutline,
+    ActiveIcon: IoCar,
+  },
+  {
+    name: "Manage Bookings",
+    href: "/manage-bookings",
+    Icon: IoTicketOutline,
+    ActiveIcon: IoTicket,
+  },
+];
+
+const superAdminNavigation: NavigationItem[] = [
+  {
+    name: "Dashboard",
+    href: "/superadmin-dashboard",
+    Icon: IoHomeOutline,
+    ActiveIcon: IoHome,
+  },
+  {
+    name: "Manage Admins",
+    href: "/manage-admins",
+    Icon: IoPersonOutline,
+    ActiveIcon: IoPersonSharp,
+  },
+  {
+    name: "Manage Vehicles",
+    href: "/manage-vehicles",
+    Icon: IoCarOutline,
+    ActiveIcon: IoCar,
+  },
+  {
+    name: "Manage Bookings",
+    href: "/manage-bookings",
+    Icon: IoTicketOutline,
+    ActiveIcon: IoTicket,
+  },
+  {
+    name: "Reports",
+    href: "/reports",
+    Icon: IoStarOutline,
+    ActiveIcon: IoStar,
+  },
+];
+
+export const NAVIGATION = {
+  public: publicNavigation,
+  user: userNavigation,
+  admin: adminNavigation,
+  superAdmin: superAdminNavigation,
+};
