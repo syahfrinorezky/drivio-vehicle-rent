aac73c1ce0005cf9612b753b3255a30249e29ee4
syahfrinorezky <syahfrinorezky@gmail.com>
2025-09-26 08:41:33 +0800

    chore: add default layout

---
+++ src/app/(superadmin)/layout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import UserLayout from "../(user)/layout";
+
+function SuperadminLayout({ children }: { children: React.ReactNode }) {
+  return <div>{children}</div>;
+}
+
+export default SuperadminLayout;
